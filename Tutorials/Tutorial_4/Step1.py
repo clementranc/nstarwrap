4dff3b79913e285634e1c04837d7c54fed218252
Clément Ranc <clement.ranc@protonmail.ch>
2022-04-14 21:54:21 +0200

    Add Tuto 4

---
+++ Tutorials/Tutorial_4/Step1.py
@@ -0,0 +1,11 @@
+#
+#                    Clément Ranc, 14 April 2022
+#
+import nstarwrap.daotools as daotools
+
+if __name__ == "__main__":
+    # We run nstarwrap to create a copy of the FITS file, 
+    # optimized for (multiprocesses and) MCMCs.
+    image_file_name = 'image.fits'
+    nstar_wrapper = daotools.NstarPythonWrapper(image=image_file_name, pickle=True)
+    ncol, nrow = nstar_wrapper.attach()
